8fc51d0c3846f9c26feddafde27c6d7caff93500
cmkadmin <cmkadmin>
2023-04-03 19:15:29 -0400

    Added the password 'password_1'

--- conf.d/wato/passwords.mk
+++ conf.d/wato/passwords.mk
@@ -1,3 +1,3 @@
 # Written by Checkmk store
 
-stored_passwords.update({'dsfgwsed': {'title': 'ew', 'comment': '', 'docu_url': '', 'password': '', 'owned_by': None, 'shared_with': []}})
+stored_passwords.update({'password_1': {'title': 'tuj', 'comment': '', 'docu_url': '', 'password': '', 'owned_by': None, 'shared_with': []}})
