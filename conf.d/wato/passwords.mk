bb9e15416e0056b83d27c80e903f425a29998f37
cmkadmin <cmkadmin>
2023-04-03 19:48:33 -0400

    Removed the password 'ww'

--- conf.d/wato/passwords.mk
+++ conf.d/wato/passwords.mk
@@ -1,3 +1,3 @@
 # Written by Checkmk store
 
-stored_passwords.update({'ww': {'title': 'rzj', 'comment': '', 'docu_url': '', 'password': '', 'owned_by': None, 'shared_with': []}, 'hzj': {'title': 'j', 'comment': '', 'docu_url': '', 'password': '', 'owned_by': None, 'shared_with': []}, 't': {'title': 't', 'comment': '', 'docu_url': '', 'password': '', 'owned_by': None, 'shared_with': []}})
+stored_passwords.update({'hzj': {'title': 'j', 'comment': '', 'docu_url': '', 'password': '', 'owned_by': None, 'shared_with': []}, 't': {'title': 't', 'comment': '', 'docu_url': '', 'password': '', 'owned_by': None, 'shared_with': []}})
