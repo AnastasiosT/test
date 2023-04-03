62b5b6f0f080239eb58b5849f1359e4cce5bcd1f
cmkadmin <cmkadmin>
2023-04-03 19:19:23 -0400

    Deleted host zr

--- conf.d/wato/hosts.mk
+++ conf.d/wato/hosts.mk
@@ -1,10 +1,7 @@
 # Created by HostStorage
 
-all_hosts += ['zr']
-
-host_tags.update({'zr': {'site': 'git', 'address_family': 'ip-v4-only', 'ip-v4': 'ip-v4', 'agent': 'cmk-agent', 'tcp': 'tcp', 'checkmk-agent': 'checkmk-agent', 'piggyback': 'auto-piggyback', 'snmp_ds': 'no-snmp', 'criticality': 'prod', 'networking': 'lan'}})
 
 # Host attributes (needed for WATO)
-host_attributes.update({'zr': {'meta_data': {'created_at': 1680563093.7046874, 'updated_at': 1680563093.7462323, 'created_by': 'cmkadmin'}}})
+host_attributes.update({})
 
 folder_attributes.update({'/wato/': {'bake_agent_package': True}})
--- conf.d/wato/passwords.mk
+++
@@ -1,3 +0,0 @@
-# Written by Checkmk store
-
-stored_passwords.update({'password_1': {'title': 'tuj', 'comment': '', 'docu_url': '', 'password': '', 'owned_by': None, 'shared_with': []}})
