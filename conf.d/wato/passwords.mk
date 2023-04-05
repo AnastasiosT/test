c6dd24be8143e1f90ed07720e11d938ad024eab4
cmkadmin <cmkadmin>
2023-04-05 19:32:03 -0400

    Added the password 'rw'

--- conf.d/wato/passwords.mk
+++ conf.d/wato/passwords.mk
@@ -1,3 +1,3 @@
 # Written by Checkmk store
 
-stored_passwords.update({'a': {'title': 't', 'comment': '', 'docu_url': '', 'password': '', 'owned_by': None, 'shared_with': []}, 'ew': {'title': 'e', 'comment': '', 'docu_url': '', 'password': '', 'owned_by': None, 'shared_with': []}})
+stored_passwords.update({'a': {'title': 't', 'comment': '', 'docu_url': '', 'password': '', 'owned_by': None, 'shared_with': []}, 'ew': {'title': 'e', 'comment': '', 'docu_url': '', 'password': '', 'owned_by': None, 'shared_with': []}, 'rw': {'title': 'r', 'comment': '', 'docu_url': '', 'password': '', 'owned_by': None, 'shared_with': []}})
