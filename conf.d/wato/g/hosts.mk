1c493e3db699a3fa9ff90ed870453adf7ce18f5b
cmkadmin <cmkadmin>
2023-04-05 17:15:50 -0400

    Modified host ame-ame-sws-prd01.arthrex.local-agent.txt.

--- conf.d/wato/g/hosts.mk
+++ conf.d/wato/g/hosts.mk
@@ -2,9 +2,9 @@
 
 all_hosts += ['ame-ame-sws-prd01.arthrex.local-agent.txt']
 
-host_tags.update({'ame-ame-sws-prd01.arthrex.local-agent.txt': {'site': 'git', 'address_family': 'no-ip', 'agent': 'no-agent', 'piggyback': 'auto-piggyback', 'snmp_ds': 'no-snmp', 'criticality': 'prod', 'networking': 'lan'}})
+host_tags.update({'ame-ame-sws-prd01.arthrex.local-agent.txt': {'site': 'git', 'address_family': 'no-ip', 'agent': 'cmk-agent', 'tcp': 'tcp', 'checkmk-agent': 'checkmk-agent', 'piggyback': 'auto-piggyback', 'snmp_ds': 'no-snmp', 'criticality': 'prod', 'networking': 'lan'}})
 
 # Host attributes (needed for WATO)
-host_attributes.update({'ame-ame-sws-prd01.arthrex.local-agent.txt': {'meta_data': {'created_at': 1680729292.0, 'created_by': 'cmkadmin', 'updated_at': 1680729338.3409293}}})
+host_attributes.update({'ame-ame-sws-prd01.arthrex.local-agent.txt': {'meta_data': {'created_at': 1680729292.0, 'created_by': 'cmkadmin', 'updated_at': 1680729350.6819947}, 'tag_agent': 'cmk-agent'}})
 
 folder_attributes.update({})
