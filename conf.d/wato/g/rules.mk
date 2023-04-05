a3116c4d5f072f3fe979aa9feebe20aa29b73fb8
cmkadmin <cmkadmin>
2023-04-05 17:15:33 -0400

    Changed properties of rule #0 in ruleset "Individual program call instead of agent access" in folder "Main / g"

--- conf.d/wato/g/rules.mk
+++ conf.d/wato/g/rules.mk
@@ -2,8 +2,7 @@
 globals().setdefault('datasource_programs', [])
 
 datasource_programs = [
-{'condition': {'host_folder': '/%s/' % FOLDER_PATH,
-               'host_name': ['ame-ame-sws-prd01.arthrex.local-agent.txt']},
+{'condition': {'host_folder': '/%s/' % FOLDER_PATH},
  'id': '04a6deb4-2ca8-4a30-9529-8e812bbabc18',
  'options': {'disabled': False},
  'value': 'cat /home/anastasios/tribe29/Checkmk/agents/$HOSTNAME$'},
