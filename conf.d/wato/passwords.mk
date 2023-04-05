8c53e15fef1a21ea22cd3c9444a398a0b110ec1e
cmkadmin <cmkadmin>
2023-04-05 08:45:11 -0400

    Removed the password 't'

--- conf.d/wato/passwords.mk
+++ conf.d/wato/passwords.mk
@@ -1,3 +1,3 @@
 # Written by Checkmk store
 
-stored_passwords.update({'t': {'title': 't', 'comment': '', 'docu_url': '', 'password': '', 'owned_by': None, 'shared_with': []}, 'a': {'title': 't', 'comment': '', 'docu_url': '', 'password': '', 'owned_by': None, 'shared_with': []}})
+stored_passwords.update({'a': {'title': 't', 'comment': '', 'docu_url': '', 'password': '', 'owned_by': None, 'shared_with': []}})
