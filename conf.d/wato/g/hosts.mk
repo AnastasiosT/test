12d7522779e315613d7f91712a4ac26126934290
cmkadmin <cmkadmin>
2023-04-05 17:14:52 -0400

    Created new host ame-ame-sws-prd01.arthrex.local-agent.txt.

---
+++ conf.d/wato/g/hosts.mk
@@ -0,0 +1,10 @@
+# Created by HostStorage
+
+all_hosts += ['ame-ame-sws-prd01.arthrex.local-agent.txt']
+
+host_tags.update({'ame-ame-sws-prd01.arthrex.local-agent.txt': {'site': 'git', 'address_family': 'no-ip', 'agent': 'no-agent', 'piggyback': 'auto-piggyback', 'snmp_ds': 'no-snmp', 'criticality': 'prod', 'networking': 'lan'}})
+
+# Host attributes (needed for WATO)
+host_attributes.update({'ame-ame-sws-prd01.arthrex.local-agent.txt': {'meta_data': {'created_at': 1680729292.0724483, 'updated_at': 1680729292.1116695, 'created_by': 'cmkadmin'}}})
+
+folder_attributes.update({})
